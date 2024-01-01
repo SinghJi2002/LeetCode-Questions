d3e462a77c462ae97f23d98b5ccff3f39be090ab
Ashutosh Kumar Singh <124508368+SinghJi2002@users.noreply.github.com>
2024-01-01 22:47:40 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (28.01%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,10 +14,12 @@
  * }
  */
 
-//Approach: First of all lets define diameter:The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+//Approach: First of all lets define diameter:The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.Jo basic approach hai wo rhega to find the height of the tree. But yha left or right ke bitch mai maximum nii krna hai, bkli yha dono ko add krna hai, of fir current maximum diameter variable se compare krke usko update krna hai.
+
 //Logic: Hr Node pe jao. Ab hr node ka left of right side height nikalo. Dono ko add kro. Ye krke hme mil gya the diameter of binary tree across that node. Ye ab hr node ke liye kro. Jiske across highest diameter aagya whi apka answer hojayga.
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
+        //Ye diameterOfTree ko array jaisa mt dekho. Iska kaam bss diameterOfTree[0] tk ka hai. Ye variable hi hr iteration pr maximum diameter store krke rkhega across all recurssion calls. Ye call hoga getHeight mai. Ye function mai right or left height calculate krke hum diameter nikal or sath hi sath compare kre jare with current max diameter.
         int[] diameterOfTree=new int[1];
         getHeight(root, diameterOfTree);
         return(diameterOfTree[0]);
