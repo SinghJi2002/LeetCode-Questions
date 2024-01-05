cdfd58a215c725a80df8741b20f750da1e90fc16
Ashutosh Kumar Singh <124508368+SinghJi2002@users.noreply.github.com>
2024-01-05 12:39:06 +0530

    Time: 1 ms (32.29%), Space: 42.2 MB (12.07%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -17,6 +17,7 @@ class Solution {
     public void flatten(TreeNode root) {
         //Iterative Solution: Approach Samjhne Ke Liye Striver Ki Ye wali video dekhlena.
         //https://www.youtube.com/watch?v=sWf7k1x9XR4&t=48s
+        //In short though, the approach is that every root's right pointer is to be connect to its left node. While at the same time the left pointer is to point to null. The general access of elements will only be controlled by stack.
         Stack<TreeNode>flatten=new Stack<>();
         if(root!=null){
             flatten.push(root);
