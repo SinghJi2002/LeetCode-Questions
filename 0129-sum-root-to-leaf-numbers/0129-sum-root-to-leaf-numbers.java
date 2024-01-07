a1b42505bc29407fadc02f24d4eb2c902be8e1bd
Ashutosh Kumar Singh <124508368+SinghJi2002@users.noreply.github.com>
2024-01-07 22:49:39 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (25.83%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,11 +14,15 @@
  * }
  */
 class Solution {
+    //Declaring a global variable so that it can be updated anywhere.
+    //Approach is to maintain two variables. value that calculates the path sum. ans that adds up the path sum. Value is updated as follows in every call whereas ans is updated only when leaf is reached. General left and right traversal will happen as normal.
     int ans=0;
     public void dfs(TreeNode root,int value){
+    
         if(root==null){
             return;
         }
+        //Mutiplication by 10 takes place inorder to perform leftward shift.
         value=value*10;
         value=value+root.val;
         if(root.right==null && root.left==null){
