2637182141587298521c454745e35df9b004be59
Ashutosh Kumar Singh <124508368+SinghJi2002@users.noreply.github.com>
2024-01-07 22:11:41 +0530

    Time: 0 ms (100.00%), Space: 43 MB (79.11%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -13,14 +13,19 @@
  *     }
  * }
  */
+
+//Approach: Try for both left and right traversal and continue decreasing targetSum. If by the time we reach a leaf node, targetSum becomes zero, well, we return true or return false.
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        //Simple case is, if the target sum does not become zero and we reach null return false.
         if(root==null){
             return(false);
         }
+        //This check whether we have reached the leaf, since reaching the leaf is important. If we reach the leaf and the targetSum becomes zero, true will be return since the condition is satisfied or else false.
         if(root.left==null && root.right==null){
             return(targetSum-root.val==0);
         }
+        //Will do both left and right traversals and check for true in either of these traversals.
         return(hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val));
     }
 }
