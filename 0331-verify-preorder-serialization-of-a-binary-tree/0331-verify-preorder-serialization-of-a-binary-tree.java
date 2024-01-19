a7b9db6e0f9f789025448e7ce8bab03bcac36f41
Ashutosh Kumar Singh <124508368+SinghJi2002@users.noreply.github.com>
2024-01-19 10:51:02 +0530

    Time: 8 ms (23.19%), Space: 43.5 MB (61.78%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -1,5 +1,16 @@
 class Solution {
-    boolean isValid=true;
+    //Refer to the video for better understanding: https://www.youtube.com/watch?v=YJjao6iPTQo
+    
+    //Though Lets try to outline the algorithm being used here.
+    /*
+    1)Take the string and create an string array out of it.
+    2)Now create a stack. When we encounter integer, push without any questions.
+    3)In case we encounter "#",check whether the top of the stack is also an "#". If yes,
+    pop the top. Then check whether the new top is integer or not. If yes, pop the top, or else 
+    let it be as it is.
+    4)After all is done, we will see the tree being reduced to a "#" in stack. If thats the case
+    return true, or else return false.
+    */
     
     public boolean isValidSerialization(String preorder) {
         Stack<String>s=new Stack<>();
