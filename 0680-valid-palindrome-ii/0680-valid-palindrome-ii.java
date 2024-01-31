60466769e6c934d33c9c446a6202ff0c0b7b5480
Ashutosh Kumar Singh <124508368+SinghJi2002@users.noreply.github.com>
2024-01-31 23:28:46 +0530

    Time: 4 ms (99.84%), Space: 45.8 MB (5.74%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    public boolean checkPalindrome(String s,int left,int right){
+        while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }
+            else{
+                return(false);
+            }
+        }
+        return(true);
+    }
+    
+    
+    public boolean validPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                right--;
+                left++;
+            }
+            else{
+                return checkPalindrome(s,left+1,right)||checkPalindrome(s,left,right-1);
+            }
+        }
+        return(true);
+    }
+}
