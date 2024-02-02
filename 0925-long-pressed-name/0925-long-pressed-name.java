c73c585a15b7e589392d7bb015d580c2c9a6d067
Ashutosh Kumar Singh <124508368+SinghJi2002@users.noreply.github.com>
2024-02-02 19:11:17 +0530

    Time: 1 ms (81.07%), Space: 41.4 MB (50.19%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int p1=0;
+        int p2=0;
+        while(p1<name.length() && p2<typed.length()){
+            if(name.charAt(p1)==typed.charAt(p2)){
+                p1++;
+                p2++;
+            }
+            else if(p1!=0 && typed.charAt(p2)==name.charAt(p1-1)){
+                p2++;
+            }
+            else{
+                return(false);
+            }
+        }
+        if(p1<name.length()){
+            return(false);
+        }
+        if(p2<typed.length()){
+            char current=typed.charAt(p2-1);
+            while(p2<typed.length()){
+                if(typed.charAt(p2)!=current){
+                    return(false);
+                }
+                p2++;
+            }
+        }
+        return(true);
+    }
+}
